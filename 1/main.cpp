06565818753974444314327f35d66986d90b1dec
Leon Monster <leon.monster@hotmail.com>
2023-01-30 23:01:39 +0100

    Day 1

--- 1/main.cpp
+++ 1/main.cpp
@@ -5,13 +5,36 @@
 using namespace std;
 
 int main(){
-    fstream = newfile;
+    fstream newfile;
     newfile.open("input.txt", ios::in); //open file in 'read' mode
     if(newfile.is_open()){ 
         string line;
+        int one = 0;
+        int two = 0;
+        int three = 0;
+
         while(getline(newfile, line)){
-            cout << line << endl;
+            int elf = 0; //total amount per elf
+            while(line != ""){
+                elf += stoi(line);
+                getline(newfile, line);
+            }
+            if(elf > one){
+                three = two;
+                two = one;
+                one = elf;
+            } else if(elf > two){
+                three = two;
+                two = elf;
+            } else if(elf > three){
+                three = elf;
+            }
+            cout << one << "  |  " << two << "  |  " << three << "  |  " << endl;
         }
+        cout << one << endl;
+        cout << two << endl;
+        cout << three << endl;
+        cout << one + two + three;
     }
     //test
     return 0;
